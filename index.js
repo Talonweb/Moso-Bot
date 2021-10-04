20e42953761356fdbc275c25cf6bf6c5d69fe47d
Talon <Nikdjakovic@hotmail.com>
2021-10-03 22:03:47 -0400

    a

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js"); 
 const ee = require("./botconfig/embed.json");
 const Nuggies = require("nuggies");
+const db = require("quick.db");
 const client = new Discord.Client({
   fetchAllMembers: false,
   restTimeOffset: 0,
@@ -35,6 +36,93 @@ Nuggies.connect(uri);
 require('discord-buttons')(client);
 Nuggies.handleInteractions(client)
 
+
+client.on('messageReactionAdd', async (reaction, user) => {
+  if(user.partial) await user.fetch();
+  if(reaction.partial) await reaction.fetch();
+  if(reaction.message.partial) await reaction.message.fetch();
+  if(user.bot) return;
+  let giveawayid = await db.get(`GiveawayEmbed_${reaction.message.id}`)
+  if(!giveawayid) return
+  let giveawayrole = await db.get(`GiveawayRole_${reaction.message.id}`)
+  if(!giveawayrole) return;
+   if(reaction.message.id == giveawayid && reaction.emoji.name == `🚀`) {
+    var home = await db.get(`giveawaydone_${reaction.message.id}`)
+     
+    var reactioncheck = setInterval(async function() {
+  
+       let member = reaction.message.guild.members.cache.get(user.id) 
+      let guild = client.guilds.cache.get(reaction.message.guild.id)
+      let role = guild.roles.cache.find(role => role.id === `${giveawayrole}`); 	      
+
+      if(!member.roles.cache.has(`${role.id}`)) { 
+        reaction.users.remove(user.id) 
+       }
+       
+ 
+if(home === null) {
+    clearInterval()
+    clearInterval(reactioncheck);
+  return;
+}
+if(!home) {
+  clearInterval()
+  clearInterval(reactioncheck);
+return;
+}
+},5000);
+let member = reaction.message.guild.members.cache.get(user.id) 
+let guild = client.guilds.cache.get(reaction.message.guild.id)
+let role = guild.roles.cache.find(role => role.id === `${giveawayrole}`)
+let ffff = new Discord.MessageEmbed()
+.setColor(ee.wrongcolor)
+.setDescription(`Your entry for [this giveaway](https://discord.com/channels/${reaction.message.guild.id}/${reaction.message.channel.id}/${reaction.message.id}) has been **Denied**. You need **${role.name}** role!`)
+  let embed = new Discord.MessageEmbed()
+  .setColor(ee.color)
+  .setDescription(`Your Entry for [this giveaway](https://discord.com/channels/${reaction.message.guild.id}/${reaction.message.channel.id}/${reaction.message.id}) has been **Approved**.
+  
+    `)
+  .setTimestamp()
+  .setFooter(ee.footertext, ee.footericon)
+if(member.roles.cache.has(`${role.id}`)) return user.send(embed)
+if(!member.roles.cache.has(`${role.id}`)) return user.send(ffff)
+}
+})
+
+client.on('messageReactionAdd', async (reaction, user) => {
+   if(user.partial) await user.fetch();
+  if(reaction.partial) await reaction.fetch();
+  if(reaction.message.partial) await reaction.message.fetch();
+  if(user.bot) return;
+  let giveawayid = await db.get(`GiveawayEmbed_${reaction.message.id}`)
+   if(!giveawayid) return
+  let giveawayids = await db.get(`GiveawayID_${reaction.message.id}`)
+  if(!giveawayids) return;
+   if(reaction.message.id == giveawayid && reaction.emoji.name == `🚀`) {
+let guild = client.guilds.cache.get(giveawayids)
+let guildcheck = guild.member(user.id)
+
+     var reactioncheck = setInterval(async function() {
+   if(!guildcheck) { return reaction.users.remove(user.id); }
+    
+    },5000)
+    if(guildcheck) {
+      let embed = new Discord.MessageEmbed()
+    .setColor(ee.color)
+    .setDescription(`Your Entry for [this giveaway](https://discord.com/channels/${reaction.message.guild.id}/${reaction.message.channel.id}/${reaction.message.id}) has been **Approved**.
+    
+      `)
+    user.send(embed) 
+           }
+           if(!guildcheck) {
+       let ffff = new Discord.MessageEmbed()
+      .setColor(ee.wrongcolor)
+      .setDescription(`Your entry for [this giveaway](https://discord.com/channels/${reaction.message.guild.id}/${reaction.message.channel.id}/${reaction.message.id}) has been **Denied**.`)
+    reaction.users.remove(user.id)
+      user.send(ffff)  
+     }
+   }
+});
 
 client.on("guildMemberAdd", function (message) {
 
